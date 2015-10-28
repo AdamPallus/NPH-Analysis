de0cfaa7847e92f465d8b02f57b395f37b2ba4d9
AdamPallus <pallusa@gmail.com>
2015-10-28 11:29:19 -0700

    minor change to model plotter test

--- PlottingNPH/modelplotter.R
+++ PlottingNPH/modelplotter.R
@@ -3,13 +3,16 @@ addmodelprediction<-function(t){
   
 t <-  mutate(t,sdflag=lag(sdf,50),time=row_number())
 m<-lm(sdflag~rep+lep,t)
-mv<-lm(sdflag~rep+lep+rev+lev,t)
+mv<-lm(sdflag~rep+lep+repV+lepV,t)
 t$posmodel<-predict(m,t)
 t$pvmodel<-predict(mv,t)
 t
 }
 
-path <- "~/GitHub/NPH-Analysis/data/"
+library(dplyr)
+library(broom)
+
+path <- "~/GitHub/NPH-Analysis/practicedata/"
 files <- list.files(path=path,pattern='*.csv')
 tt<- data.frame()
 for (i in 1:length(files)) {
@@ -18,6 +21,8 @@ for (i in 1:length(files)) {
   t<-addmodelprediction(t)
   tt<- rbind(tt,t)
 }
+
+library(ggplot2)
 
 ggplot(filter(tt,time<50000))+
 geom_area(aes(time,sdflag))+
