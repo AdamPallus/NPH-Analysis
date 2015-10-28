35c9d4240d548d78074f0c4e54a1c40d36d11415
AdamPallus <pallusa@gmail.com>
2015-10-27 17:44:38 -0700

    More bootstrap and modeling

---
+++ PlottingNPH/modelplotter.R
@@ -0,0 +1,25 @@
+
+addmodelprediction<-function(t){
+  
+t <-  mutate(t,sdflag=lag(sdf,50),time=row_number())
+m<-lm(sdflag~rep+lep,t)
+mv<-lm(sdflag~rep+lep+rev+lev,t)
+t$posmodel<-predict(m,t)
+t$pvmodel<-predict(mv,t)
+t
+}
+
+path <- "~/GitHub/NPH-Analysis/data/"
+files <- list.files(path=path,pattern='*.csv')
+tt<- data.frame()
+for (i in 1:length(files)) {
+  t <- read.csv(paste(path,files[i],sep=''))
+  t$neuron<-gsub('.csv','',files[i])
+  t<-addmodelprediction(t)
+  tt<- rbind(tt,t)
+}
+
+ggplot(filter(tt,time<50000))+
+geom_area(aes(time,sdflag))+
+geom_line(aes(time,pvmodel),color="green")+
+geom_line(aes(time,posmodel),color="red")+facet_grid(neuron~.)
