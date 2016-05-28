87906d8dc9df7959fa01b9574797bd398da8356e
AdamPallus <pallusa@gmail.com>
2016-05-27 17:16:47 -0700

    model improvements

--- Vergence/whatistheburst.R
+++ Vergence/whatistheburst.R
@@ -10,30 +10,53 @@ library(ggplot2)
 library(manipulate)
 
 t<-readRDS('SOA-NRTP.RDS')
-z<- filter(t,neuron=='Bee-25')
+z<- filter(t,neuron=='Bee-06')
 # m<- mm$m[[1]] #from above summary: model based on static FR ~ verg.angle
 
 # z<- mutate(z,time=row_number(),
 #            expectedFR= predict(m,newdat=z))
 
-z<- mutate(z,time=row_number(),
-           sdf=spikedensity(rasters,sd=30),
-           verg.velocity=parabolicdiff(verg.angle,20),
-           transient.type='none',
-       transient.type=replace(transient.type, verg.velocity > 15,'convergence'),
-       transient.type=replace(transient.type, verg.velocity < -15, 'divergence'),
-       verg.velocity.fixed=replace(verg.velocity, transient.type=='divergence',verg.velocity*-1))
+# z<- mutate(z,time=row_number(),
+#            sdf=spikedensity(rasters,sd=30),
+#            verg.velocity=parabolicdiff(verg.angle,20),
+#            transient.type='none',
+#        transient.type=replace(transient.type, verg.velocity > 15,'convergence'),
+#        transient.type=replace(transient.type, verg.velocity < -15, 'divergence'),
+#        verg.velocity.fixed=replace(verg.velocity, transient.type=='divergence',verg.velocity*-1))
 
-
-#split verg.velocity into positive and negative
 z %>%
-  mutate(verg.velocity.positive=replace(verg.velocity,verg.velocity<0,0),
-         verg.velocity.negative=replace(verg.velocity,verg.velocity>0,0),
-         verg.velocity.negative=abs(verg.velocity.negative),
-         is.transient.positive=abs(verg.velocity.positive)>15,
-         is.transient.negative=abs(verg.velocity.negative)>15) ->
+  mutate(time=row_number(),
+         sdf=spikedensity(rasters,sd=30),
+         verg.velocity=parabolicdiff(verg.angle,30),
+         # verg.accel=parabolicdiff(verg.velocity,20),
+         verg.direction=verg.angle>0) ->
   z
 
+z<- left_join(z,markEnhancement(z$verg.velocity),by='time')
+
+z %>%
+  mutate(verg.enhance=!is.na(enhancenum),
+         transient.type='none',
+         verg.direction=verg.velocity>0)->
+  z
+
+i<- z$verg.enhance & !z$verg.direction
+z$transient.type[i]<- 'divergence'
+i<- z$verg.enhance & z$verg.direction
+z$transient.type[i]<- 'convergence'
+
+z<- mutate(z,is.convergent.trans=transient.type=='convergence')
+
+
+##split verg.velocity into positive and negative
+# z %>%
+#   mutate(verg.velocity.positive=replace(verg.velocity,verg.velocity<0,0),
+#          verg.velocity.negative=replace(verg.velocity,verg.velocity>0,0),
+#          verg.velocity.negative=abs(verg.velocity.negative),
+#          is.transient.positive=abs(verg.velocity.positive)>15,
+#          is.transient.negative=abs(verg.velocity.negative)>15) ->
+#   z
+# 
 
 
 # z$expectedFR<- replace(z$expectedFR,z$expectedFR<0,0)
@@ -43,7 +66,6 @@ z %>%
 # z$adjustedFR<- replace(x$adjustedFR,x$adjustedFR<0,0)
 
 
-rnum<- c(0, 5, 10,15, 20,25, 30, 35, 40,45, 50, 55, 60)
 rnum<- seq(0,250,by=5)
 m1<- rnum
 m2<- rnum
@@ -52,7 +74,8 @@ for (i in rnum) {
   count<- count+1
   z<- mutate(z,sdflead=lag(sdf,i))
   mcontrol<- lm(sdflead~verg.angle+verg.velocity,data=z)
-  mtest<- lm(sdflead~verg.angle+verg.velocity:transient.type,data=z)
+  # mtest<- lm(sdflead~verg.angle+verg.velocity:transient.type,data=z)
+  mtest<- lm(sdflead~verg.angle+verg.velocity:is.convergent.trans,data=filter(z,!transient.type=='divergence'))
   m1[count]=summary(mcontrol)$r.squared
   m2[count]=summary(mtest)$r.squared
   # print(summary(mcontrol)$r.squared)
@@ -68,37 +91,59 @@ z<- mutate(z,sdflead=lag(sdf,bestlag))
 # z<- mutate(z, sdflead=lag(sdf,150))
 mcontrol<- lm(sdflead~verg.angle+verg.velocity,data=z)
 # mtest<- lm(sdflead~verg.angle+verg.velocity:transient.type,data=z)
-mtest<- lm(sdflead~verg.angle+verg.velocity.positive:is.transient.positive+verg.velocity.negative,data=z)
+# mtest<- lm(sdflead~verg.angle+verg.velocity.positive:is.transient.positive+verg.velocity.negative,data=z)
 
 # z<- mutate(z, verg.vel.smooth=as.numeric(smooth(verg.velocity,kind='3RS3R')))
 # mtest<- lm(sdflead~verg.angle+verg.vel.smooth:transient.type,data=z)
+# mtest<-lm(sdflead~verg.angle+verg.velocity:transient.type,data=z)
+mtest<- lm(sdflead~verg.angle+verg.velocity:is.convergent.trans,data=filter(z,!transient.type=='divergence'))
+# mtest<-lm(sdflead~verg.angle+verg.velocity:transient.type,data=filter(z,!transient.type=='divergence'))
 
-
+# zz<- mutate(z,transient.type=replace(transient.type,transient.type=='divergence','none'))
 z<- mutate(z,econtrol=predict(mcontrol,newdata=z),
            etest=predict(mtest,newdata=z),
+           etest=replace(etest,etest<0,0),
            verg.angle.shifted=lag(verg.angle,bestlag),
            showrasters=replace(rasters,rasters<1,NA))
 
+etest.prob<- z$etest/1000
 
+
+z <-mutate(z,predict.spikes1=rbinom(length(etest.prob),1,etest.prob),
+           predict.spikes1=replace(predict.spikes1,predict.spikes1<1,NA),
+           predict.spikes2=rbinom(length(etest.prob),1,etest.prob),
+           predict.spikes2=replace(predict.spikes2,predict.spikes2<1,NA),
+           predict.spikes3=rbinom(length(etest.prob),1,etest.prob),
+           predict.spikes3=replace(predict.spikes3,predict.spikes3<1,NA))
+
+r.squared=cor(z$sdflead[200:nrow(z)],z$etest[200:nrow(z)])
+
+print(paste('R-Squared is:',round(r.squared,3)))
 
 maxtime<-nrow(z)
-windowsize<- 1000
+windowsize<- 5000
 
 manipulate(ggplot(filter(z,time>window,time<window+windowsize))+
              geom_area(aes(time,sdflead))+
-             geom_line(aes(time,econtrol),color='orange')+
+             # geom_line(aes(time,econtrol),color='orange')+
              geom_area(aes(time,etest),fill='purple',alpha=.5)+
              geom_hline(yintercept=-115)+
              geom_hline(yintercept=-85)+
              
              geom_line(aes(time,verg.velocity-100),color='red')+
+             geom_point(aes(time,verg.velocity-100),color='red',size=3,data=filter(z, time>window,time<window+windowsize,enhancenum>0))+
              # geom_line(aes(time,verg.vel.smooth-100),color='black')+
              
              geom_point(aes(time,showrasters+200),shape='|',size=2)+
+             geom_point(aes(time,predict.spikes1+220),shape='|',size=2,color='magenta')+
+             geom_point(aes(time,predict.spikes2+225),shape='|',size=2,color='magenta')+
+             geom_point(aes(time,predict.spikes3+230),shape='|',size=2,color='magenta')+
+             
+             geom_point(aes(time,enhancenum*0-50,color=transient.type))+
              
            # geom_line(aes(time,rep*10),color='red')+
            # geom_line(aes(time,lep*10),color='blue'),
-           geom_line(aes(time,verg.angle.shifted*3.3),color='darkgreen',size=1.5),
+           geom_line(aes(time,verg.angle.shifted*5),color='darkgreen',size=1.5),
            window=slider(0,maxtime-windowsize,step=windowsize)
 )
 
